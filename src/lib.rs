37265e2fcc12d3560118f4275e9a2c974936916d
Richard Dodd (dodj) <richard.o.dodd@gmail.com>
2020-09-02 09:32:56 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -147,6 +147,7 @@ pub fn read_choice(
             Ok(a) => {
                 if a < 1 {
                     println!("{} is not a valid option (too small)", a);
+                    continue;
                 }
                 let ans = a - 1;
                 if ans < options.len() {
